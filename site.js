eae6e1aec9e73ab42b58eb1c2def94d9bce01ac4
Guilherme Duarte <gui14511@gmail.com>
2024-05-20 17:43:11 -0300

    Ajuste layout do portifólio

--- site.js
+++ site.js
@@ -50,4 +50,48 @@ document.addEventListener("DOMContentLoaded", function() {
     menuIcon.addEventListener('click', () => {
         menu.classList.toggle('active');
     });
+
+    // Fade in animation on scroll
+    const faders = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+        threshold: 0,
+        rootMargin: "0px 0px -100px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(
+        entries,
+        appearOnScroll
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add("appear");
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+
+    // Smooth scrolling for menu links
+    const links = document.querySelectorAll('nav .menu a');
+
+    for (const link of links) {
+        link.addEventListener('click', clickHandler);
+    }
+
+    function clickHandler(e) {
+        e.preventDefault();
+        const href = this.getAttribute("href");
+        const offsetTop = document.querySelector(href).offsetTop;
+
+        scroll({
+            top: offsetTop,
+            behavior: "smooth"
+        });
+    }
 });
